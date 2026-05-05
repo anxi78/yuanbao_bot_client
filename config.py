61de123da1102000bd0587451da476d9e9caa8a9
anxi78 <anxi_xg@qq.com>
2026-05-05 12:50:02 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# 元宝 Bot 配置
-# 请在这里填写你的 Bot 凭证
-
-# Token 格式: appKey:appSecret
-APP_KEY = ""
-APP_SECRET = ""
-
-API_DOMAIN = "bot.yuanbao.tencent.com"
-WS_URL = "wss://bot-wss.yuanbao.tencent.com/wss/connection"
